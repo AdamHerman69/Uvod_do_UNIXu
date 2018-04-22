f034bddd6e504ac6f93fcf7928a12fe512c94033
AdamHerman69 <36764766+AdamHerman69@users.noreply.github.com>
2018-04-22 15:43:22 +0200

    Update uloha04.sh

--- uloha04.sh
+++ uloha04.sh
@@ -1,1 +1,4 @@
-necovoe
+# 1) Sliju vsechny seznamy do jednoho, vypisu duplikaty s poctem opakovani, vyfiltruji ty, ktere se nachazi 3x 
+# a odeberu z vystupu pocet vyskytu
+
+sort social.txt beverly_hills.txt actor.txt | uniq -cd | grep 3 | sed 's/.*3 //'
