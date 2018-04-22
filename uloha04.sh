f26e77f50536510b2fcbb1d1d1cd983a086cb786
AdamHerman69 <36764766+AdamHerman69@users.noreply.github.com>
2018-04-22 17:48:03 +0200

    Update uloha04.sh

--- uloha04.sh
+++ uloha04.sh
@@ -7,3 +7,17 @@ sort social.txt beverly_hills.txt actor.txt | uniq -cd | grep 3 | sed 's/.*3 //'
 
 grep -oh "\".*\"" countrycodes_en.csv kodyzemi_cz.csv | tr -d '"' | sort | uniq -d
 
+# 3)
+
+cut -d: -f 1,4 /etc/group | { 
+while read line; 
+do 
+echo $line | sed 's/[,:]/ /g' | wc -w | tr -d '\n'; 
+echo " $line"; 
+done; 
+} | sort -nr | { 
+pocetSlov=$(head -c1);
+pocetRadku=`expr $(grep -c $pocetSlov) + 1` ;
+echo $pocetRadku ; }
+
+pocetSlov=$(head -c1 seznam); pocetRadku=`expr $(grep -c $pocetSlov seznam )` ; grep $pocetRadku seznam | grep -o ' .*:' | tr -d ':' | tr -d ' '
