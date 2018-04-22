d79bbbddfda825a6b5fdddbf3e61b41ce2c1bde3
AdamHerman69 <36764766+AdamHerman69@users.noreply.github.com>
2018-04-22 16:32:49 +0200

    Update uloha04.sh

--- uloha04.sh
+++ uloha04.sh
@@ -2,3 +2,8 @@
 # a odeberu z vystupu pocet vyskytu
 
 sort social.txt beverly_hills.txt actor.txt | uniq -cd | grep 3 | sed 's/.*3 //'
+
+# 2) Grepem vytáhnu z .csv souborů názvy zemí, odeberu uvozovky, setřídim a nechám vyjet duplikáty.
+
+grep -oh "\".*\"" countrycodes_en.csv kodyzemi_cz.csv | tr -d '"' | sort | uniq -d
+
