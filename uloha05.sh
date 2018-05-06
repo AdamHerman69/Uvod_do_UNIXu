f99037ab89da642fd7c8d9995aeec50bd5810aff
AdamHerman69 <36764766+AdamHerman69@users.noreply.github.com>
2018-05-06 21:32:53 +0200

    Update uloha05.sh

--- uloha05.sh
+++ uloha05.sh
@@ -8,13 +8,17 @@ ls -lSh /etc | head -n 2 | tail -n 1
 
 find /var -type f -printf "%s\t%p\n" |sort -n | tail -n 1
 
-// vyhledá (rekursivně) všechny regulární soubory, a vypíše každý na řádek společně s jeho velikostí | numericky je seřadí a vybere poslední
+// vyhledá (rekurzivně) všechny regulární soubory, a vypíše každý na řádek společně s jeho velikostí | numericky je seřadí a vybere poslední
 
 3)
 
 find /etc -type f -printf "%T@\t%p\n" | sort -n | tail -n 1
 
-// vyhledá (rekursivně) všechny regulární soubory, a vypíše každý na řádek společně s časem jeho poslední modifikace (v sekundách od počátku času) | numericky je seřadí a vybere poslední
+// vyhledá (rekurzivně) všechny regulární soubory, a vypíše každý na řádek společně s časem jeho poslední modifikace (v sekundách od počátku času) | numericky je seřadí a vybere poslední
+
+find /etc -maxdepth 1 -type f -printf "%T@\t%p\n" | sort -n | tail -n 1
+
+// to samé akorát nastavíme maximální hloubku rekurze 1
 
  4)
  
