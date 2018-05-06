f8e06e0bce302820711e9e1bd2c4d2bb792323fa
AdamHerman69 <36764766+AdamHerman69@users.noreply.github.com>
2018-05-06 21:26:43 +0200

    Update uloha05.sh

--- uloha05.sh
+++ uloha05.sh
@@ -1,1 +1,21 @@
+1)
 
+ls -lSh /etc | head -n 2 | tail -n 1
+
+// vypíše řádku i s inforacemi o souboru, pokud bychom chtěli jen jméno souboru museli bychom to z ní ještě vytáhnout
+
+2)
+
+find /var -type f -printf "%s\t%p\n" |sort -n | tail -n 1
+
+// vyhledá (rekursivně) všechny regulární soubory, a vypíše každý na řádek společně s jeho velikostí | numericky je seřadí a vybere poslední
+
+3)
+
+find /etc -type f -printf "%T@\t%p\n" | sort -n | tail -n 1
+
+// vyhledá (rekursivně) všechny regulární soubory, a vypíše každý na řádek společně s časem jeho poslední modifikace (v sekundách od počátku času) | numericky je seřadí a vybere poslední
+
+ 4)
+ 
+ 
